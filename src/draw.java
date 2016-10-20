645c461e9419feabf22f66dbaa0f64c75dbd129e
Roderyx <Roderyx@192.168.1.4>
2016-10-20 17:32:43 -0600

    Para datos de grafica

--- src/draw.java
+++ src/draw.java
@@ -1,4 +1,32 @@
 
 public class draw {
-
+	public double total;
+	public double ocio;
+	public double servicios;
+	public double comida;
+	public double estudios;
+	
+	public void setTotal(double ocio, double servicios, double comida, double estudios){
+		total = ocio+ servicios+ comida + estudios;
+	}
+	
+	public double getDatosOcio(double ocio){
+		double porcentaje= (ocio/total)*100;
+		return porcentaje;
+	}
+	
+	public double getDatoServicio(double servicio){
+		double porcentaje = (servicio/total)*100;
+		return porcentaje;
+	}
+	
+	public double getDatoComida(double comida){
+		double porcentaje= (ocio/total)*100;
+		return porcentaje;
+	}
+	
+	public double getDatoEstudios(double estudios){
+		double porcentaje= (estudios/total)*100;
+		return porcentaje;
+	}
 }
