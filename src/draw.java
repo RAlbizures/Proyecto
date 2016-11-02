67180da0de98da97a09e935c61f1c02e36d89cf1
Josrodjr <Josrodjr@gmail.com>
2016-11-02 08:29:19 -0600

    Documentacion de la clase draw

--- src/draw.java
+++ src/draw.java
@@ -1,3 +1,13 @@
+/**
+*Clase de gastos de una persona
+*@author: Javier Andres Ramos Galvez 16230 
+*@author: Rodrigo Stuardo Juarez Jui 16073 
+*@author: Rodrigo Javier Albizures Lopez 16767
+*@author: Jose Rodolfo Perez Garcia 16056
+*@version: 11/2/2016
+*/
+
+
 
 public class draw {
 	
@@ -6,7 +16,9 @@ public class draw {
 	public double servicios;
 	public double comida;
 	public double estudios;
-
+/**
+*Constructor de la clase draw 
+*/
 	public draw(double ocio, double servicios, double comida, double estudios) {
 		this.total = 0;
 		this.ocio = ocio;
@@ -14,15 +26,22 @@ public class draw {
 		this.comida = comida;
 		this.estudios = estudios;
 	}
-	
+/**
+*metodo que saca el dinero total en double de todos los gastos
+*/	
 	public void setTotal(){
 		total = ocio+ servicios+ comida + estudios;
 	}
-	
+/**
+*metodo que regresa el total de los gastos
+*/	
 	public double getTotal(){
 		return total;
 	}
-	
+/**
+*Metodo que regresa el porcentaje de dinero total
+*@return Double de porcentaje
+*/	
 	public double getPorcentaje(double d){
 		double porcentaje= (d/total)*100;
 		return porcentaje;
