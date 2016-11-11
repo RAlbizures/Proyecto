f601389d780508d0a4d7dbe1c04cc4e4e4ed0028
Rodrigo Juárez <Rodrigo Jurez>
2016-11-10 20:31:37 -0600

    Mensajes de error funcionales

--- src/graph.java
+++ src/graph.java
@@ -226,6 +226,15 @@ public class graph {
 					 			    "Error",
 					 			    JOptionPane.ERROR_MESSAGE);
 					 	}
+					 	catch(NumberFormatException e1){
+					 		e1.printStackTrace();
+					 		System.err.println("El valor ingresado no es un numero");
+					 		JOptionPane.showMessageDialog(frame,
+					 			    "El valor ingresado no es un numero",
+					 			    "Error",
+					 			    JOptionPane.ERROR_MESSAGE);
+					 	}
+					 	
 			 } 	
 			 if (e.getSource() == btnGraficar) {
 				 	JFrame grafica = new JFrame("");
@@ -268,6 +277,14 @@ public class graph {
 		    				    "Error",
 		    				    JOptionPane.ERROR_MESSAGE);
 		    	 }
+		    	 catch(NumberFormatException e1){
+				 		e1.printStackTrace();
+				 		System.err.println("El valor ingresado no es un numero");
+				 		JOptionPane.showMessageDialog(frame,
+				 			    "El valor ingresado no es un numero",
+				 			    "Error",
+				 			    JOptionPane.ERROR_MESSAGE);
+				 	}
 		        }
 		
 		}
